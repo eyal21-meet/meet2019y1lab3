2714355e373ac0ab6579a6b87b1934dc3451d625
eyal21-meet <eyal21@meet.mit.edu>
2019-07-23 05:31:13 -0400

    Stopping here

---
+++ bonus 3 age difference.py
@@ -0,0 +1,5 @@
+pers1 = input("what is the age of the first person? ")
+pers2 = input("what is the age of the second person? ")
+
+age_difference = abs(float(pers1) - float(pers2))
+print("The age diffecence is " + str(age_difference) + " years.")
